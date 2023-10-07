95df51f77d054bb041cc19aab3ecef963daf678d
NigarValikhanova <140783772+NigarValikhanova@users.noreply.github.com>
2023-10-07 22:36:16 +0400

    07.10.2023 MT

---
+++ 07.10.2023 MT/Program.cs
@@ -0,0 +1,42 @@
+﻿
+namespace _07._10._2023_MT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region My Method
+            //Dictionary<string, int> wordToNumber = new Dictionary<string, int>()
+            //{
+            //{"zero", 0}, {"one", 1}, {"two", 2}, {"three", 3},
+            //{"four", 4}, {"five", 5}, {"six", 6}, {"seven", 7},
+            //{"eight", 8}, {"nine", 9}
+            //};
+            //Console.Write("Enter a number with words: ");
+            //string userInput = Console.ReadLine().ToLower();
+
+            //string[] words = userInput.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            //int result = 0;
+            //foreach (string word in words)
+            //{
+            //    if (wordToNumber.ContainsKey(word))
+            //    {
+            //        result = result * 10 + wordToNumber[word];
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"Invalid word: {word}");
+            //        return;
+            //    }
+            //}
+            //Console.WriteLine("Integer value: " + result);
+
+            #endregion
+
+            #region
+
+            #endregion
+        }
+    }
+}
